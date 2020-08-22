e49ee23281e08adabc59186a70e334e968f1f702
Alexander Eldeib <alexeldeib@gmail.com>
2020-08-22 01:39:18 -0700

    add sku capability test cases
    Signed-off-by: Alexander Eldeib <alexeldeib@gmail.com>

--- sku_test.go
+++ sku_test.go
@@ -1,6 +1,7 @@
 package skewer
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2020-06-01/compute"
@@ -8,9 +9,166 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_SKU_HasCapability(t *testing.T) {}
+func Test_SKU_HasCapability(t *testing.T) {
+	cases := map[string]struct {
+		sku        compute.ResourceSku
+		capability string
+		expect     bool
+	}{
+		"empty capability should not match empty sku": {
+			sku:        compute.ResourceSku{},
+			capability: "",
+		},
+		"empty capability should not match sku with empty list of capabilities": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{},
+			},
+			capability: "",
+		},
+		"empty capability should not match when present and false": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr(""),
+						Value: to.StringPtr("False"),
+					},
+				},
+			},
+			capability: "",
+		},
+		"empty capability should not match when present and weird value": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr(""),
+						Value: to.StringPtr("foobar"),
+					},
+				},
+			},
+			capability: "",
+		},
+		"foo capability should not match when false": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr("foo"),
+						Value: to.StringPtr("False"),
+					},
+				},
+			},
+			capability: "foo",
+		},
+		"foo capability should match when true": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr("foo"),
+						Value: to.StringPtr("True"),
+					},
+				},
+			},
+			capability: "foo",
+			expect:     true,
+		},
+	}
 
-func Test_SKU_HasCapabilityWithCapacity(t *testing.T) {}
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			if diff := cmp.Diff(tc.expect, SKU(tc.sku).HasCapability(tc.capability)); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func Test_SKU_HasCapabilityWithCapacity(t *testing.T) {
+	cases := map[string]struct {
+		sku        compute.ResourceSku
+		capability string
+		capacity   int64
+		expect     bool
+		err        error
+	}{
+		"empty capability should not match empty sku": {
+			sku:        compute.ResourceSku{},
+			capability: "",
+		},
+		"empty capability should not match sku with empty list of capabilities": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{},
+			},
+			capability: "",
+		},
+		"empty capability should error when present and weird value": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr(""),
+						Value: to.StringPtr("foobar"),
+					},
+				},
+			},
+			capability: "",
+			err:        fmt.Errorf("failed to parse string 'foobar' as int64: strconv.ParseInt: parsing \"foobar\": invalid syntax"),
+		},
+		"empty capability should  match when present with zero capacity and requesting zero": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr(""),
+						Value: to.StringPtr("0"),
+					},
+				},
+			},
+			capability: "",
+			expect:     true,
+		},
+		"foo capability should not match when present and less than capacity": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr("foo"),
+						Value: to.StringPtr("100"),
+					},
+				},
+			},
+			capability: "foo",
+			capacity:   200,
+		},
+		"foo capability should match when true": {
+			sku: compute.ResourceSku{
+				Capabilities: &[]compute.ResourceSkuCapabilities{
+					{
+						Name:  to.StringPtr("foo"),
+						Value: to.StringPtr("10"),
+					},
+				},
+			},
+			capability: "foo",
+			capacity:   5,
+			expect:     true,
+		},
+	}
+
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			got, err := SKU(tc.sku).HasCapabilityWithCapacity(tc.capability, tc.capacity)
+			if tc.err != nil {
+				if diff := cmp.Diff(tc.err.Error(), err.Error()); diff != "" {
+					t.Error(diff)
+				}
+			} else {
+				if diff := cmp.Diff(tc.expect, got); diff != "" {
+					t.Error(diff)
+				}
+			}
+		})
+	}
+}
 
 func Test_SKU_IsResourceType(t *testing.T) {
 	cases := map[string]struct {
